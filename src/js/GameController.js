80329df419def99c628c305bcf49af898bf68ed5
vymat <vymatvienko@gmail.com>
2023-10-05 14:42:46 +0300

    Changes added

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,24 +1,24 @@
 export default class GameController {
-    constructor(gamePlay) {
-      this.gamePlay = gamePlay;
-    }
-  
-    getCell() {
-      return Math.floor(
-        Math.random() * (this.gamePlay.boardSize * this.gamePlay.boardSize)
-      );
-    }
-  
-    init() {
-      this.gamePlay.drawUi();
-  
-      const newsInterval = setInterval(() => {
-        let newCell = this.getCell();
-        while (newCell === this.gamePlay.activeCell) {
-          newCell = this.getCell();
-        }
-        this.gamePlay.redrawPositions(newCell);
-        this.gamePlay.activeCell = newCell;
-      }, 1000);
-    }
+  constructor(gamePlay) {
+    this.gamePlay = gamePlay;
+  }
+
+  getCell() {
+    return Math.floor(
+      Math.random() * (this.gamePlay.boardSize * this.gamePlay.boardSize),
+    );
+  }
+
+  init() {
+    this.gamePlay.drawUi();
+
+    setInterval(() => {
+      let newCell = this.getCell();
+      while (newCell === this.gamePlay.activeCell) {
+        newCell = this.getCell();
+      }
+      this.gamePlay.redrawPositions(newCell);
+      this.gamePlay.activeCell = newCell;
+    }, 1000);
+  }
 }
